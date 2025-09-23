d0c08830196748f14ff9e6250bbb486c2719adfd
Trung Vo <trungvo@Trungs-MacBook-Pro.local>
2025-09-23 20:41:59 +0700

    avatar

--- src/components/users/view.user.detail.jsx
+++ src/components/users/view.user.detail.jsx
@@ -1,9 +1,26 @@
 import { Drawer } from "antd";
+import { useState } from "react";
 
 const ViewUserDetail = (props) => {
   // nhận props từ thằng cha của nó
   const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
+  const [selectFile, setSelectFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+  const handleOnChangeFile = (event) => {
+    if (!event.target.files || event.target.files.length === 0) {
+      setSelectFile(null);
+      setPreview(null);
+      return;
+    }
 
+    // I've kept this example simple by using the first image instead of multiple
+    const file = event.target.files[0];
+    if (file) {
+      setSelectFile(file);
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+  console.log(">>>Check file: ", preview);
   return (
     <>
       <Drawer
@@ -86,8 +103,43 @@ const ViewUserDetail = (props) => {
                 }>
                 Đổi ảnh đẹp hơn
               </label>
-              <input type="file" hidden id="inputForm" />
+              {/* file là 1 loại dữ liệu đặc biệt, khác với object hay array
+               */}
+              <input
+                type="file"
+                hidden
+                id="inputForm"
+                onChange={(event) => {
+                  handleOnChangeFile(event);
+                }}
+              />
             </div>
+            {preview && (
+              <div
+                style={{
+                  marginTop: "10px",
+                  height: "120px",
+                  width: "120px",
+                  border: "2px dashed #00bfff", // viền xanh da trời
+                  borderRadius: "12px", // bo góc
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#f9f9f9", // nền sáng
+                  overflow: "hidden",
+                  boxShadow: "0 2px 6px rgba(0,0,0,0.1)", // bóng nhẹ
+                }}>
+                <img
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    objectFit: "cover",
+                  }}
+                  src={preview}
+                  alt=""
+                />
+              </div>
+            )}
           </>
         ) : (
           <>
